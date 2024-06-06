0fd0f5134877471486ea9339c8002507e32f880f
IFISOLUTION\ndphong1 <phong.nguyendinh@emeal.nttdata.com>
2024-06-06 17:32:23 +0700

    part 16

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
     before_action :authenticate_user!
     def index
-        @tweets = Tweet.include(:user).order(created_at: :desc).map { |tweet| TweetPresenter.new(tweet) }
+        @tweets = Tweet.order(created_at: :desc).map { |tweet| TweetPresenter.new(tweet) }
     end
 end
